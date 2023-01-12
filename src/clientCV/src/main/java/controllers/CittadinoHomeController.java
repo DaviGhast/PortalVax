9995c7635e84012416d69ee3d9f9dfaec358ed55
lucamuggiasca <lucamuggiasca@outlook.com>
2023-01-12 16:37:48 +0100

    Added CittadinoHome and InfoCentro GUI

---
+++ src/clientCV/src/main/java/controllers/CittadinoHomeController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class CittadinoHomeController {
+    public void InfoCentro(ActionEvent actionEvent) throws IOException {
+        MainClientUIController.setRoot("infocentro");
+    }
+
+    public void registratiACentro(ActionEvent actionEvent) {
+    }
+
+    public void InserireEventiAvversi(ActionEvent actionEvent) {
+    }
+
+    public void RecuperaIDVaccinazione(ActionEvent actionEvent) {
+    }
+}
---
+++ src/clientCV/src/main/java/controllers/InfoCentroController.java
@@ -0,0 +1,34 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+public class InfoCentroController {
+    public void validatorChoiseBox(ActionEvent actionEvent) {
+    }
+
+    public void validatorfield2(KeyEvent keyEvent) {
+    }
+    public void validatoreNomeCentro(ActionEvent actionEvent) {
+    }
+    public void CercaCentro(ActionEvent actionEvent) {
+    }
+    public void viewRegexNomeCentro(MouseEvent mouseEvent) {
+    }
+
+    public void viewRegex2(MouseEvent mouseEvent) {
+    }
+
+    public void viewRegex0(MouseEvent mouseEvent) {
+    }
+
+    public void torna_indietro(ActionEvent actionEvent) throws IOException {
+        MainClientUIController.setRoot("cittadino_home");
+    }
+
+
+
+}
